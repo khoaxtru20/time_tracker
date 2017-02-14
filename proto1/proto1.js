f3eff4409a481f303b50cedabb436dcd23d7d6d5
Khoa Truong <Khoa Truong>
2017-02-13 23:29:49 -0600

    Updated sessionStorage functionality

--- proto1/proto1.js
+++ proto1/proto1.js
@@ -1,37 +1,47 @@
 //Value in the form
 var val = "";
 
-// if (!sessionStorage.getItem("db")){
-//   console.log("no storage");
-// }
-// else {
-//   console.log("var in storage");
-// }
-//
-var test_display = document.getElementById("test");
-// console.log(saved);
-// sessionStorage.setItem("db", saved);
-//
-// console.log(sessionStorage.getItem("db"));
 
-if (sessionStorage.getItem("db")) {
-  var saved = sessionStorage.getItem("db");
-  test_display.innerHTML = saved;
+if(document.getElementById("test")){
+
+  var test_display = document.getElementById("test");
+
+  if (sessionStorage.getItem("db")) {
+    var saved = sessionStorage.getItem("db");
+    test_display.innerHTML = saved;
+  }
 }
 
 
-//Get the form from the html
-var x = document.getElementById("input");
-//Update the value of the form
-x.onkeypress = function(ev) {keypress(ev)};
-
-function keypress(ev) {
-  val += String.fromCharCode(ev.which);
+if(document.getElementById("input")){
+  //Get the form from the html
+  var x = document.getElementById("input");
+  //Update the value of the form
+  x.onkeydown = function(ev) {keydown(ev)};
 }
+
+function keydown(ev) {
+  if (ev.which == 8) {
+    val = val.substring(0, val.length-1);
+  }
+  else {
+    if(ev.which != 13 && ev.which != 16){
+      if (!ev.shiftKey && ev.which >= 65 && ev.which <= 90){
+        val += String.fromCharCode(ev.which+32);
+      }
+      else{
+        val += String.fromCharCode(ev.which);
+      }
+    }
+  }
+  console.log(val);
+}
+
 $(document).ready(function() {
   //Advance to Current Activity Page if form is filled, else show warning
     $(".track").click(function() {
         if (val){
+          sessionStorage.setItem("db", val);
           location.href = "./currActivity.html";
         }
         else {
