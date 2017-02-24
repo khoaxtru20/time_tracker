e15fdd175ad923ab8fcd336e8794639f9e35117b
Khoa Truong <Khoa Truong>
2017-02-23 22:33:46 -0600

    You can hit enter to Add Task.

--- proto3_USE/todo.js
+++ proto3_USE/todo.js
@@ -1,30 +1,28 @@
+list = [];
 function submit() {
+
   var messy_list = $('form').serializeArray();
-  var list = [];
 
   messy_list.forEach( function(e) {
     if (e.value != "") {
       list.push(e.value);
     }
   });
+
   sessionStorage.setItem("list", JSON.stringify(list));
   location.href = "./todo_listed.html";
 
 }
+document.onkeypress = (function (e) {
+  if (e.which == 13){
+    submit();
+  }
+});
 
-function submitEnter() {
-  var messy_list = $('form').serializeArray();
-  var list = [];
-
-  messy_list.forEach( function(e) {
-    if (e.value != "") {
-      list.push(e.value);
-    }
-  });
-  sessionStorage.setItem("list", JSON.stringify(list));
-  location.href = "./todo_listed.html";
-
+function updateStorage() {
+  // sessionStorage.setItem("db", JSON.stringify(list));
 }
+
 //Only do this on the page with this element (todo_listed.html)
 i=0;
 if(document.getElementById("input"+i)){
