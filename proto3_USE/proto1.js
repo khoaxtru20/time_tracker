a5262b7aa1c72b23f1d22fe30c00cc102f17d84f
Khoa Truong <Khoa Truong>
2017-02-26 17:46:31 -0600

    Simplified proto1.js

--- proto3_USE/proto1.js
+++ proto3_USE/proto1.js
@@ -1,89 +1,40 @@
-//Value in the form
-var val = "";
+main = [];
+function track() {
 
-//Only do this on the page with this element (currActivity.html)
-if(document.getElementById("currAct")){
+  var messy_list = $('form').serializeArray();
 
-  var test_display = document.getElementById("currAct");
+  messy_list.forEach( function(e) {
+    if (e.value != "") {
+      main.push(e.value);
+    }
+  });
+  sessionStorage.setItem("db", JSON.stringify(main));
+  location.href = "./currActivity.html";
 
-  if (sessionStorage.getItem("db")) {
-    var saved = sessionStorage.getItem("db");
-    test_display.innerHTML = saved;
-  }
 }
-//Only do this on the page with this element (homepage_timeline.html)
-if(document.getElementById("timeline")){
-
-  var timeLine = document.getElementById("timeline");
-
-  if (sessionStorage.getItem("db")) {
-    var saved = sessionStorage.getItem("db");
-    timeLine.innerHTML = saved;
+document.onkeypress = (function (e) {
+  if (e.which == 13){
+    e.preventDefault();
+    track();
   }
-}
 
+});
+//Set storage value to element;
+function setStorage(id){
 
+  if(document.getElementById(id)){
 
-//Only do this on the pages with this element (index.html)
-if(document.getElementById("input")){
-  //Get the form from the html
-  var x = document.getElementById("input");
-  //Update the value of the form
-  x.onkeydown = function(ev) {keydown(ev)};
-}
+    var setEl = document.getElementById(id);
 
-//**NOTE: breaks on symbols that are only accessible with a key + shift e.g. exclamation point
-function keydown(ev) {
-  //If backspace is pressed, delete the last element in string
-  if (ev.which == 8) {
-    val = val.substring(0, val.length-1);
-  }
-  else {
-    //As long as shift and enter keys aren't pressed initially:
-    if(ev.which != 13 && ev.which != 16){
-      //And the keys are a-z
-      if (!ev.shiftKey && ev.which >= 65 && ev.which <= 90){
-        //Print lowercase (since default is capital for the onkeydown event)
-        val += String.fromCharCode(ev.which+32);
-      }
-      else{
-        //Print everything else
-        val += String.fromCharCode(ev.which);
-      }
+    if (sessionStorage.getItem("db")) {
+      var saved = JSON.parse(sessionStorage.getItem("db"));
+      setEl.innerHTML = saved[0];
     }
   }
-  // console.log(val);
 }
 
-$(document).ready(function() {
-  //Advance to Current Activity Page if form is filled, else show warning
-    $(".track").click(function() {
-        if (val){
-          sessionStorage.setItem("db", val);
-          location.href = "./currActivity.html";
-        }
-        else {
-              $(".alert").show();
-        }
-    });
-    //Advance to Current Activity Page by pressing Enter Key if form is filled, else show warning
-    $("input").keypress(function(event) {
-      if (event.which == 13) {
-        event.preventDefault();
-        if (val){
-          sessionStorage.setItem("db", val);
-          location.href = "./currActivity.html";
-        }
-        else {
-          $(".alert").show();
-        }
-      }
-    });
-//Click on the warning to make it go away
-  $(".alert").click(function(){
-    $(".alert").hide();
-  });
-});
+setStorage("currAct");
+setStorage("timeline");
 
 //Set Date on TimeLine
 function gDate() {
@@ -91,6 +42,5 @@ function gDate() {
   var dateEl = document.getElementById('date');
   // 'Setting' data-attributes using setAttribute
   dateEl.setAttribute('data-date',d.toDateString());
-  console.log(d);
   return;
 }
